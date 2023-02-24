3b53b355c1d5c8fbaf4baeab38b3019148611c12
Aleksandr Iurashchuk <a.iurashchuk@gmail.com>
2023-02-24 20:39:19 +0100

    renamed namespace + onPlayerActivated()

--- Dancer.lua
+++ Dancer.lua
@@ -14,18 +14,18 @@ local ranks = {
 }
 local maxRank = ranks[7]
 
--- create a namespace for HWS by declaring a top-level table that will hold everything else.
-if HWS == nil then HWS = {} end
+-- create a namespace for Dancer by declaring a top-level table that will hold everything else.
+if Dancer == nil then Dancer = {} end
 
 -- The AddOn name
-HWS.name = "Dancer"
-HWS.version = "1.0"
+Dancer.name = "Dancer"
+Dancer.version = "1.0"
 
-HWS.settings = {}
+Dancer.settings = {}
 
 -- default values for saved variables
 -- see http://wiki.esoui.com/AddOn_Quick_Questions#How_do_I_save_settings_on_the_local_machine.3F
-HWS.defaults = {
+Dancer.defaults = {
 	alpha = 50,
 	x = 40,
 	y = 450,
@@ -34,7 +34,7 @@ HWS.defaults = {
 }
 
 local function GetRank(dances)
-	-- plus one because Lua starts indexing at 1 *facepalm.gif*
+	-- plus one because Lua starts indexing at 1
 	local rank = math.floor(dances / 30) + 1
 	local res = ranks[rank]
 	if res ~= nil then
@@ -44,42 +44,47 @@ local function GetRank(dances)
 	end
 end
 
-function HWS.OnCombatEvent(eventCode, ActionResult, isError, abilityName, abilityGraphic, ActionSlotType, sourceName, sourceType, targetName, targetType, hitValue, powerType, damageType, log, sourceUnitId, targetUnitId, abilityId, overflow)
+function Dancer.OnCombatEvent(eventCode, ActionResult, isError, abilityName, abilityGraphic, ActionSlotType, sourceName, sourceType, targetName, targetType, hitValue, powerType, damageType, log, sourceUnitId, targetUnitId, abilityId, overflow)
 	if IsBlockActive() then
 		danceCounter = danceCounter + 1
-		HWS.RefreshWindow(danceCounter, GetRank(danceCounter))
+		Dancer.RefreshWindow(danceCounter, GetRank(danceCounter))
 	end
 end
 
-function HWS.RefreshWindow(counter, rankTst)
-	HWS.PopulateWindow("Dance moves performed: "..counter, "Rank: "..rankTst)
+function Dancer.OnPlayerActivated()
+	em:UnregisterForEvent("Dancer_Start", EVENT_PLAYER_ACTIVATED)
+	Dancer.RefreshWindow(danceCounter, GetRank(danceCounter))
+end
+
+function Dancer.RefreshWindow(counter, rankTst)
+	Dancer.PopulateWindow("Dance moves performed: "..counter, "Rank: "..rankTst)
 end
 
 --
 -- This function that will initialize our addon with ESO
 --
-function HWS.Initialize(event, addon)
-    -- filter for just HWS addon event
-	if addon ~= HWS.name then return end
+function Dancer.Initialize(event, addon)
+    -- filter for just Dancer addon event
+	if addon ~= Dancer.name then return end
 	danceCounter = 0
 
 	em:UnregisterForEvent("DancerInitialize", EVENT_ADD_ON_LOADED)
 
 	-- load our saved variables
-	HWS.settings = ZO_SavedVars:New("DancerSavedVars", 1, nil, HWS.defaults)
+	Dancer.settings = ZO_SavedVars:New("DancerSavedVars", 1, nil, Dancer.defaults)
 
 	-- make a label for our keybinding
 	ZO_CreateStringId("SI_BINDING_NAME_DANCER_TOGGLE", "Toggle Window")
 
 	-- make our options menu
-	HWS.MakeMenu()
+	Dancer.MakeMenu()
 
-	em:RegisterForEvent("Dancer_CombatEvent", EVENT_COMBAT_EVENT, function(...) HWS.OnCombatEvent(...) end)
+	em:RegisterForEvent("Dancer_CombatEvent", EVENT_COMBAT_EVENT, function(...) Dancer.OnCombatEvent(...) end)
 	em:AddFilterForEvent("Dancer_CombatEvent", EVENT_COMBAT_EVENT, REGISTER_FILTER_ABILITY_ID, 55146)
-	em:RegisterForEvent("Dancer_Start", EVENT_PLAYER_ACTIVATED, function(...) HWS.RefreshWindow(danceCounter, GetRank(danceCounter)) end)
+	em:RegisterForEvent("Dancer_Start", EVENT_PLAYER_ACTIVATED, function(...) Dancer.OnPlayerActivated() end)
 end
 
 
 
 -- register our event handler function to be called to do initialization
-em:RegisterForEvent("DancerInitialize", EVENT_ADD_ON_LOADED, function(...) HWS.Initialize(...) end)
+em:RegisterForEvent("DancerInitialize", EVENT_ADD_ON_LOADED, function(...) Dancer.Initialize(...) end)
